6b6b36de8041a659795dd5d0e321bb5b022561c0
David Liu <davidliu2117@gmail.com>
2025-04-16 13:44:03 -0700

    added room page

---
+++ client/src/pages/room.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Room = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1>Video Room</h1>
+      <p>Your video meeting space</p>
+    </div>
+  );
+};
+
+export default Room;
