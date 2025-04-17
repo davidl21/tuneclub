18ed935b849e14c7a02aa6cb7cb67d6c6266a777
David Liu <davidliu2117@gmail.com>
2025-04-16 20:16:17 -0700

    added room to browserrouter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import Room from "./pages/room.tsx";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/room",
+    element: <Room />,
   },
 ]);
 
