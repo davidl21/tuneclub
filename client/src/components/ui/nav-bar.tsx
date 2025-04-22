821c6cd1633aa2aa2d73f9c9cd229a3a64ea8bb4
David Liu <davidliu2117@gmail.com>
2025-04-21 18:01:33 -0700

    set inter as global font

--- client/src/components/ui/nav-bar.tsx
+++ client/src/components/ui/nav-bar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav className="w-full py-2 px-6 border-b border-zinc-800">
+    <nav className="sticky top-0 w-full py-2 px-6 border-b border-zinc-800 bg-zinc-900 z-50">
       <div className="flex justify-between items-center px-4">
         <Link to="/" className="text-xl font-semibold text-white">
           TuneClub
